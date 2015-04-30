8a918795471797bb9369b5e44245a49f919ad8da
serginho10 <scscf.1992@gmail.com>
2015-04-30 00:11:54 -0500

    actualizacion

---
+++ Tasmc/app/src/main/java/com/example/vivanco/tasmc/InfoVuelo.java
@@ -0,0 +1,122 @@
+package com.example.vivanco.tasmc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class InfoVuelo extends ActionBarActivity {
+
+    private ViewPager mPager;
+    private SlidingTabLayout mTabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_vuelo);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar_infv);
+        setSupportActionBar(toolbar);
+
+        //Habilita el boton para ir a la actividad principal en el Toolbar
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setDistributeEvenly(true);
+        mTabs.setViewPager(mPager);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        //Si el id seleccionado es igual al del home regresa a la principal
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        String[] tabs;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabs = getResources().getStringArray(R.array.tabv);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            if (position == 0) {
+                fragment = new LlegadaNacional();
+            }
+            if (position == 1) {
+                fragment = new LlegadaInternacional();
+            }
+            if (position == 2) {
+                fragment = new SalidaNacional();
+            }
+            if (position == 3) {
+                fragment = new SalidaInternacional();
+            }
+
+            return fragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    }
+
+    public static class MyFragment extends Fragment {
+        public static MyFragment getInstance(int position) {
+            MyFragment fragment = new MyFragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.fragment_llegada_nacional, container, false);
+            return layout;
+        }
+    }
+
+    //Funcion para buscar por numero de vuelo
+    public void buscar() {
+
+
+    }
+}

6bd123611d4393cc58ca41ec9bf587202f14a068
erickvica <erickvivanco01@hotmail.com>
2015-05-05 14:02:50 -0500

    renglonvuelosdisponibles actualizado

--- Tasmc/app/src/main/java/com/example/vivanco/tasmc/VuelosDisponibles.java
+++ Tasmc/app/src/main/java/com/example/vivanco/tasmc/VuelosDisponibles.java
@@ -1,13 +1,20 @@
 package com.example.vivanco.tasmc;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 
-public class VuelosDisponibles extends ActionBarActivity {
+public class VuelosDisponibles extends ActionBarActivity implements View.OnClickListener, SwipeRefreshLayout.OnRefreshListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,44 @@ public class VuelosDisponibles extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void buildFAB() {
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.ic_action_new);
+
+        //Creacion del boton flotante
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                .setContentView(imageView)
+                .setBackgroundDrawable(R.drawable.selector_button_red)
+                .build();
+
+        //Creacion de las opciones del boton flotante
+
+        ImageView iconFecha = new ImageView(this);
+        iconFecha.setImageResource(R.drawable.ic_action_calendar);
+        ImageView iconClase = new ImageView(this);
+        iconClase.setImageResource(R.drawable.ic_action_important);
+        ImageView iconPrecio = new ImageView(this);
+        iconPrecio.setImageResource(R.drawable.precio);
+        //Subopciones
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+        //Background de presionado para los sub botones
+        itemBuilder.setBackgroundDrawable(getResources().getDrawable(R.drawable.selector_sub_button_gray));
+        SubActionButton btnFecha = itemBuilder.setContentView(iconFecha).build();
+        SubActionButton btnClase = itemBuilder.setContentView(iconClase).build();
+        SubActionButton btnPrecio = itemBuilder.setContentView(iconPrecio).build();
+
+        //Action al presionar
+        btnFecha.setOnClickListener(this);
+        btnClase.setOnClickListener(this);
+        btnPrecio.setOnClickListener(this);
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(btnFecha)
+                .addSubActionView(btnClase)
+                .addSubActionView(btnPrecio)
+                .attachTo(actionButton)
+                .build();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,5 +87,15 @@ public class VuelosDisponibles extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 }

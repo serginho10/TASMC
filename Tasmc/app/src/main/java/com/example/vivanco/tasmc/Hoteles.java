0e4d3de8d57b069b43dd1875eefe80381c3dfdd5
serginho10 <scscf.1992@gmail.com>
2015-06-02 00:40:32 -0500

    nueva hoteles

--- Tasmc/app/src/main/java/com/example/vivanco/tasmc/Hoteles.java
+++ Tasmc/app/src/main/java/com/example/vivanco/tasmc/Hoteles.java
@@ -16,11 +16,8 @@ import android.widget.Spinner;
 public class Hoteles extends ActionBarActivity implements View.OnClickListener {
 
     Spinner huespedes;
-    Spinner habitaciones;
     Spinner categorias;
     EditText lugar;
-    EditText fechain;
-    EditText fechaout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,60 +27,29 @@ public class Hoteles extends ActionBarActivity implements View.OnClickListener {
         setSupportActionBar(toolbar);
 
         lugar = (EditText) findViewById(R.id.lugar);
-        fechain = (EditText) findViewById(R.id.fechain);
-        fechaout = (EditText) findViewById(R.id.fechaout);
 
         //Habilita el boton para ir a la actividad principal en el Toolbar
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        String[] array;
+
         //spinner huespedes
+        array = getResources().getStringArray(R.array.personas);
         huespedes = (Spinner) findViewById(R.id.huespedes);
-        ArrayAdapter dataAdapter = ArrayAdapter.createFromResource(this, R.array.personas, android.R.layout.simple_spinner_item);
+        ArrayAdapter dataAdapter = new ArrayAdapter(this, R.layout.spinner_text_layout ,array);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         huespedes.setAdapter(dataAdapter);
 
-        //spinner habitaciones
-        habitaciones = (Spinner) findViewById(R.id.habitaciones);
-        ArrayAdapter dataAdapter1 = ArrayAdapter.createFromResource(this, R.array.habitaciones, android.R.layout.simple_spinner_item);
-        dataAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        habitaciones.setAdapter(dataAdapter1);
-
         //spinner hotel estrellas
+        array = getResources().getStringArray(R.array.estrellas);
         categorias = (Spinner) findViewById(R.id.categoria);
-        ArrayAdapter dataAdapter2 = ArrayAdapter.createFromResource(this, R.array.estrellas, android.R.layout.simple_spinner_item);
+        ArrayAdapter dataAdapter2 = new ArrayAdapter(this, R.layout.spinner_text_layout ,array);
         dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorias.setAdapter(dataAdapter2);
 
     }
-
-    public void onStart() {
-        super.onStart();
-
-        fechain.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    DateDialog dialog = new DateDialog(v);
-                    FragmentTransaction ft = getFragmentManager().beginTransaction();
-                    dialog.show(ft, "DatePicker");
-                }
-            }
-        });
-        fechaout.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    DateDialog dialog = new DateDialog(v);
-                    FragmentTransaction ft = getFragmentManager().beginTransaction();
-                    dialog.show(ft, "DatePicker");
-                }
-            }
-        });
-
-    }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -113,9 +79,6 @@ public class Hoteles extends ActionBarActivity implements View.OnClickListener {
         Intent intent = new Intent(this, HotelesDisponibles.class);
         //Obtenemos los valores para pasarlos a la siguiente actividad
         intent.putExtra("lugar", lugar.getText().toString());
-        intent.putExtra("checkin", fechain.getText().toString());
-        intent.putExtra("checkout", fechaout.getText().toString());
-        intent.putExtra("habitaciones", habitaciones.getSelectedItem().toString());
         intent.putExtra("huespedes", huespedes.getSelectedItem().toString());
         intent.putExtra("estrellas", categorias.getSelectedItem().toString());
         startActivity(intent);

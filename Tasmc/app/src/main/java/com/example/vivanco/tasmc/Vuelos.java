ce84da38a60cc252cfe4c910f180809b4d314d16
serginho10 <scscf.1992@gmail.com>
2015-06-14 01:40:23 -0500

    arreglos de layout a búsqueda de vuelo

--- Tasmc/app/src/main/java/com/example/vivanco/tasmc/Vuelos.java
+++ Tasmc/app/src/main/java/com/example/vivanco/tasmc/Vuelos.java
@@ -1,9 +1,11 @@
 package com.example.vivanco.tasmc;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,12 +14,18 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import org.json.JSONException;
 
-public class Vuelos extends ActionBarActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Vuelos extends ActionBarActivity implements View.OnClickListener{
 
     Spinner clase;
     EditText origen;
     EditText destino;
-    DatePicker fecha;
+    EditText fecha;
+    DatePickerDialog dialogoFecha;
+    SimpleDateFormat formatoFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +59,22 @@ public class Vuelos extends ActionBarActivity {
             clase.setSelection(2);
         else if(claseUsuario.compareTo("Primera") == 0)
             clase.setSelection(3);
+
+        formatoFecha = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        fecha = (EditText) findViewById(R.id.etFecha);
+        fecha.setInputType(InputType.TYPE_NULL);
+        fecha.requestFocus();
+        fecha.setOnClickListener(this);
+
+        Calendar calendarioNuevo = Calendar.getInstance();
+        dialogoFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar fechaNueva = Calendar.getInstance();
+                fechaNueva.set(year, monthOfYear, dayOfMonth);
+                fecha.setText(formatoFecha.format(fechaNueva.getTime()));
+            }
+        }, calendarioNuevo.get(Calendar.YEAR), calendarioNuevo.get(Calendar.MONTH), calendarioNuevo.get(Calendar.DAY_OF_MONTH));
     }
 
     @Override
@@ -78,7 +102,7 @@ public class Vuelos extends ActionBarActivity {
         }else if(destino.getText().length() == 0){
             destino.setError("Debes ingresar tu ciudad de destino.");
         }else if(fecha.toString().length() == 0){
-
+            fecha.setError("Debes ingresar la fecha de tu vuelo.");
         }else{
             JSONParser json = new JSONParser(this,getApplicationContext());
             try{
@@ -91,5 +115,10 @@ public class Vuelos extends ActionBarActivity {
                 System.out.println(json.vuelos.get(i).toString());
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        dialogoFecha.show();
     }
 }
